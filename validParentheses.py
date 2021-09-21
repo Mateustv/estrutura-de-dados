8fc94b38b2be72685a6b860a546c724f5735d037
Mateus Tavares <mateustavarestv@hotmail.com>
2021-09-21 13:17:21 -0300

    resolvendo o problema de validParentes

---
+++ validParentheses.py
@@ -0,0 +1,15 @@
+def solution(str_to_validate):
+    stack = []
+    mapping = {
+    ')' : '(',
+    ']' : '[',
+    '}' : '{'
+    }
+
+    for s in str_to_validate:
+        if s in mapping:
+            if len(stack) and mapping[s] == stack.pop():
+                continue
+        stack.append(s)
+
+    return  len(stack) == 0
