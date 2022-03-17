425e16337ca0e6b3910c3002df7ad01b7679eae0
Mateus T <mateustavarestv@hotmail.com>
2022-03-17 20:07:24 -0300

    Implementando o myAtoi resolvido

---
+++ string-to-integer-(atoi).py
@@ -0,0 +1,57 @@
+# Implement the myAtoi(string s) function, which converts a
+# string to a 32-bit signed integer (similar to C/C++'s atoi function).
+#
+# The algorithm for myAtoi(string s) is as follows:
+#
+# Read in and ignore any leading whitespace.
+# Check if the next character (if not already at the end of the string)
+# is '-' or '+'. Read this character in if it is either. This determines if
+# the final result is negative or positive respectively. Assume the result is positive if neither is present.
+# Read in next the characters until the next non-digit character or the end of the input is
+# reached. The rest of the string is ignored.
+# Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32).
+# If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+# If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp
+# the integer so that it remains in the range. Specifically, integers less than -231 should
+# be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+# Return the integer as the final result.
+# Note:
+#
+# Only the space character ' ' is considered a whitespace character.
+# Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+
+def sring_to_integer(s):
+    numberStr = ''
+    teste = ''
+    jump = 0
+    sinal = 1
+    for check in s:
+        if check == ' ':
+            print('passou')
+            jump += 1
+            continue
+        else:
+            break
+
+    if jump < len(s) and s[jump] == '+':
+        sinal = 1
+        jump += 1
+    elif jump < len(s) and s[jump] == '-':
+        sinal = -1
+        jump += 1
+
+    for number in range(jump, len(s)):
+        if s[number].isdigit():
+            teste += s[number]
+            print('tt')
+            if (int(teste) <= -2 ** 31) or (int(teste) >= 2 ** 31 - 1):
+                return pow(2,31) - 1  if sinal == 1 else  -pow(2,31)
+            else:
+                numberStr += s[number]
+        else:
+            break
+    ans = 0
+    ans = int(numberStr)*sinal
+    return ans
+
+sring_to_integer('1000000')
